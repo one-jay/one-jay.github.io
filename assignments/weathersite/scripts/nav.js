ed7d2116f015049606f1a6298f5524e967a6fe73
Clayton Thomas <ct@Claytons-Air.T-mobile.com>
2018-06-27 13:47:36 -0700

    other scripts

---
+++ assignments/weathersite/scripts/nav.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    console.log("it worked!");
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
---
+++ assignments/weathersite/scripts/windchill.js
@@ -0,0 +1,16 @@
+//Inputs
+var high = Number(document.getElementById("high").value);
+var low  = Number(document.getElementById("low").value);
+var wind = Number(document.getElementById("wind").value);
+/*  Processing
+The formula to calculate the wind chill factor is:
+f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
+where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.
+*/
+var avgTemp = (high + low) /2;
+console.log("avgTemp= " + avgTemp);
+
+var windChill = 35.74 + (0.6215 * avgTemp)  - (35.75 * Math.pow(wind, 0.16)) + (0.4274 * avgTemp * Math.pow(wind, 0.16));
+console.log('windChill= ' + windChill);
+//Output
+document.getElementById("windChillOut").innerHTML = windChill.toFixed(1);
